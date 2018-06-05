6d84294e96647dfc20f709ec0cc90be828154662
DESKTOP-74ERUTD\Jorge del Angel <soporte@sistemasags.com>
2018-06-05 13:10:16 -0500

    Se inciai el servicio auitomaticamente e inicia un servicio de X-box como prueba.

--- WindowsService/Service1.cs
+++ WindowsService/Service1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,13 +17,40 @@ namespace WindowsService
         {
             InitializeComponent();
         }
-
+        public class service
+        {
+            public string name { get; set; }
+            public string time { get; set; }
+        }
         protected override void OnStart(string[] args)
         {
+            System.IO.File.Create(AppDomain.CurrentDomain.BaseDirectory + "Incio.txt");
+            //Instancia de ServiceController, permite controlar el estado y las acciones relacionadas a los estados
+            ServiceController sc = new ServiceController();
+            sc.ServiceName = "XboxGipSvc"; //Nombre de nuestro servicio
+            //Console.WriteLine("The xbgm service status is currently set to {0}", sc.Status.ToString()); //Checando Status
+            if (sc.Status == ServiceControllerStatus.Stopped)
+            {
+                // Iniciar el servicio si está detenido
+                //Console.WriteLine("Starting the", sc.ServiceName," service...");
+                try
+                {
+                    // Se inicia el servicio y espera hasta que su estado sea "Running" o En Ejecucion
+                    sc.Start();
+                    sc.WaitForStatus(ServiceControllerStatus.Running);
+                    // Comprobamos el estado
+                    //Console.WriteLine("The Alerter service status is now set to {0}.", sc.Status.ToString());
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine("Could not start the service.");
+                }
+            }
         }
 
         protected override void OnStop()
         {
+            //Do something
         }
     }
 }
